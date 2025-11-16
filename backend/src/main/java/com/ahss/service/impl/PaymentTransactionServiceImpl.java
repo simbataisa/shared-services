f2e56060acb6f910d795ee6e67e8c31f2813573a
Tuan Anh (Dennis) Dao <simbataisa@gmail.com>
2025-11-16 20:51:23 +0700

    ci(docker): increase resource limits to prevent OOM kills
    Increase CPU and memory limits for services in all docker-compose files

--- backend/src/main/java/com/ahss/service/impl/PaymentTransactionServiceImpl.java
+++ backend/src/main/java/com/ahss/service/impl/PaymentTransactionServiceImpl.java
@@ -21,7 +21,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
